d78c7e03732880bbbb439c2abd311aa74960b756
Anshika Kumari <anshika2912@Anshikas-MacBook-Air-2.local>
2025-12-22 21:57:24 +0530

    updates

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAqKiUYPHAr5IZJxmzXERBH4Fysw_WyZmM",
+  authDomain: "krishimitra-in.firebaseapp.com",
+  projectId: "krishimitra-in",
+  storageBucket: "krishimitra-in.firebasestorage.app",
+  messagingSenderId: "358772049936",
+  appId: "1:358772049936:web:c9e0d15e907216519fa295",
+  measurementId: "G-X0ZCQFY7E6"
+};
